bb883031c16525474291c72364a1bdc657d8f04c
arya-s <arya.darc@gmail.com>
2013-07-10 05:55:10 +0900

    Express barebone.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node imoutoweb.js
---
+++ imoutoweb.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+app.get('/', function(request, response){
+	response.send('My little bot can\'t be this functional.');
+});
+var port = process.env.PORT || 5000;
+app.listen(port, function(){
+	console.log('Listening on ', port);
+});
