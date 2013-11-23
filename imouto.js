08080276951a704a042c014b390d4f8f38025c7e
arya-s <andrei.borza@gmail.com>
2013-11-23 20:52:41 +0900

    *Testing github api.

--- imouto.js
+++ imouto.js
@@ -15,8 +15,39 @@ var bot = new irc.Client(
   }
 );
 
+//TODO TRACKER
+var http = require('http');
+
 bot.addListener('message', function(nick, to, text, message) {
-  if(text.indexOf('hi imouto') !== -1){
-    bot.say(to, 'hi2u2');
+  if(text.indexOf('.todo') !== -1){
+    var options = 'https://api.github.com/search/code?q=new+Point+repo:arya-s/GestureRecognizer';
+
+    http.get(options, function(red){
+      var output = ''
+
+      res.setEncodig('utf-8');
+
+      res.on('data', function(chunk){
+        output += chunk;
+      });
+
+      res.on('end', function(){
+        var response;
+
+        try{
+          response = JSON.parse(output).response;
+        }
+        catch(e){
+          console.log('Invalid request. ',e);
+        }
+
+        if(!response || response.success !== 1){
+          console.log('Unsuccessful call to api.');
+        }
+        else{
+          console.log(response);
+        }
+      })
+    });
   }
 });
