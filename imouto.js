1d669bb40773d597bb4cbef70ef97f50cac8f0a1
arya-s <andrei.borza@gmail.com>
2013-12-06 17:04:15 +0900

    Removed git test stuff.

--- imouto.js
+++ imouto.js
@@ -1,7 +1,7 @@
 var config = {
   botname: process.env.BOTNAME || 'imouto2',
   server: process.env.IRCSERVER || 'irc.quakenet.org',
-  channels: ((process.env.DEBUG || false) == true) ? '#babodebug' : (process.env.CHANNELS || '#babodebug').split(';')
+  channels: ((process.env.DEBUG || false) == true) ? '#NKB' : (process.env.CHANNELS || '#NKB').split(';')
 };
 var irc = require('irc');
 var bot = new irc.Client(
@@ -18,27 +18,7 @@ var bot = new irc.Client(
 var https = require('https');
 
 bot.addListener('message', function(nick, to, text, message) {
-  if (text.indexOf('.todo') !== -1) {
-    var options = {
-      host: 'api.github.com',
-      path: '/search/code?q=new+Point+repo:arya-s/GestureRecognizer',
-      headers: {
-        accept: 'application/vnd.github.v3.text-match+json'
-      }
-    };
-
-    https.get(options, function(res) {
-      var data = "";
-      res.setEncoding('utf8');
-
-      res.on('data', function (chunk) {
-        data += chunk;
-      });
-
-      res.on('end', function () {
-        json = JSON.parse(data); 
-        console.log(JSON.stringify(json));
-        });
-    });
+  if(text === "hi imouto"){
+    bot.say(to, "Hi "+nick+".");
   }
 });
