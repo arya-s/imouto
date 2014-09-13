d9f9f2a50fc741cc65d9c07ef4913368f61188b4
arya-s <andrei.borza@gmail.com>
2014-09-14 03:51:23 +0900

    Intrigue's delay.'

--- imouto.js
+++ imouto.js
@@ -1,24 +1,34 @@
 var config = {
-  botname: process.env.BOTNAME || 'imouto2',
-  server: process.env.IRCSERVER || 'irc.quakenet.org',
-  channels: ((process.env.DEBUG || false) == true) ? '#NKB' : (process.env.CHANNELS || '#NKB').split(';')
+    botname: process.env.BOTNAME || 'imouto2',
+    server: process.env.IRCSERVER || 'irc.quakenet.org',
+    channels: ((process.env.DEBUG || false) == true) ? '#babodebug' : (process.env.CHANNELS || '#babodebug').split(';')
 };
 var irc = require('irc');
 var bot = new irc.Client(
-  config.server,
-  config.botname, {
-    channels: config.channels,
+        config.server,
+        config.botname,
+        {
+            channels: config.channels,
     debug: true,
     floodProtection: true,
     floodProtectionDelay: 1000
-  }
-);
+        }
+        );
 
-//TODO TRACKER
-var https = require('https');
+//generating diff delay each time between 1-10 seconds
+function randomResponseTime() {
+    var delayInMilliseconds = Math.random();
+    delayInMilliseconds = delayInMilliseconds*10000;
+    return delayInMilliseconds;
+}
 
 bot.addListener('message', function(nick, to, text, message) {
-  if(text === "hi imouto"){
-    bot.say(to, "Hi "+nick+".");
-  }
+    //actually delaying responses
+    var delay = randomResponseTime();
+    setTimeout(function() {
+        bot.say(to, 'this is a delayed response of ' + delay);
+    }, delay);
+    if(text.indexOf('hi imouto') !== -1){
+        bot.say(to, 'hi2u2');
+    }
 });
